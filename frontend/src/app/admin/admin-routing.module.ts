202cb51e38f381790862a74fefbf4d9df9dd6baa
Laurent Schwitter <laurent.schwitter@k-lab.ch>
2024-03-06 23:31:20 +0100

    secure admin route

--- frontend/src/app/admin/admin-routing.module.ts
+++ frontend/src/app/admin/admin-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { AuthGuard } from '../shared/guards/auth.guard'
 import { EventsComponent } from './events/events.component'
 
 const routes: Routes = [
   {
     path: '',
-    children: [{ path: '', component: EventsComponent }],
+    children: [
+      { path: '', canActivate: [AuthGuard], component: EventsComponent },
+    ],
   },
 ]
 
